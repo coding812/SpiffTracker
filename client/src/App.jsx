5732f322691cd29347345806a916b50769697abe
unknown <coding812@gmail.com>
2024-05-22 23:49:31 -0400

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Record from "./components/Record";
+
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <Navbar />
+      <Record />
+      {/* <Outlet /> */}
+    </div>
+  );
+};
+export default App;
