57ecb42ce659f15c413b37630a16595a0936799f
unknown <coding812@gmail.com>
2024-05-23 14:18:53 -0400

    comment

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-// This will help us connect to the database
+// Importing the MongoDB connection from the connection file
 import db from "../db/connection.js";
 
 // This help convert the id from string to ObjectId for the _id.
