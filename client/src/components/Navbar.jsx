646b2b319413639db4472f54dca6bbe5c071514e
unknown <coding812@gmail.com>
2024-06-04 15:03:27 -0400

    Cleaned up code all into one useEffect() and added conditional rendering of navbar button on login screen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,25 +3,18 @@ import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const location = useLocation();
+  const [linkText, setLinkText] = useState("");
+  const [linkPath, setLinkPath] = useState("");
+  const [buttonVisible, setButtonVisible] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     setLoggedIn(!!token);
-  }, []);
-
-  let linkText;
-  let linkPath;
-
-  if (loggedIn) 
-  {
-    linkText = "Sign Out";
-    linkPath = "/";
-  } 
-  else 
-  {
-    linkText = "Sign In";
-    linkPath = "/login";
-  }
+    setButtonVisible(location.pathname === "/login" ? false : true);
+    setLinkPath(loggedIn ? "/" : "/login");
+    setLinkText(loggedIn ? "Sign Out" : "Sign In");
+  }, [loggedIn, location]);
 
   function handleLogout() {
     localStorage.removeItem("accessToken");
@@ -34,12 +27,14 @@ export default function Navbar() {
 
   return (
     <div className="">
+      {buttonVisible ? (
       <nav className="flex justify-end items-center mb-6">
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to={linkPath}
               onClick={() => loggedIn ? handleLogout() : handleLogin()}>
           {linkText}
         </NavLink>
       </nav>
+      ) : null}
     </div>
   );
 }
