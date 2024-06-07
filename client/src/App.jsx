83a0ace8c1eeac87aa43567119f4d5bee626aecf
unknown <coding812@gmail.com>
2024-06-07 15:26:53 -0400

    Added footer with copyright and link to personal site

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="w-full p-6">
       <Navbar />
       <Outlet />
+      <Footer />
       <ToastContainer />
     </div>
   );
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-end items-center h-16 bg-background  text-md ">
+        <p>© 2024 <a href="https://www.stevemillertime.com/" target="_blank" className="text-red-500 hover:underline">Steve Miller</a></p>
+        </footer>
+    );
+}
