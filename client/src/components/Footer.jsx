eef88c0383aa756416bf6356c322f0bffa0b8384
unknown <coding812@gmail.com>
2024-06-11 13:34:58 -0400

    added contact info in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,8 @@
 
 export default function Footer() {
     return (
-        <footer className="flex justify-end items-center h-16 bg-background text-md">
+        <footer className="flex justify-between items-center h-16 bg-background text-lg">
+            <p><a href="Mailto:coding812@gmail.com" className="text-red-500 hover:underline">Contact</a></p>
         <p>© 2024 <a href="https://www.stevemillertime.com/" target="_blank" className="text-red-500 hover:underline">Steve Miller</a></p>
         </footer>
     );
