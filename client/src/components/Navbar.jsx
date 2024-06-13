03d1a6d534402a072419113bc249de752c6b5cd9
unknown <coding812@gmail.com>
2024-06-13 11:58:47 -0400

    Made home and about buttons render on login page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,6 @@ export default function Navbar() {
 
   return (
     <div className="">
-      {buttonVisible ? (
       <nav className="flex items-center mb-6">
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3 mr-3" to={(loggedIn ? '/admin' : '/')}>
           Home
@@ -33,13 +32,14 @@ export default function Navbar() {
         </NavLink>
 
         <div className="flex-grow"></div>
+      {buttonVisible ? (
 
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to={linkPath}
               onClick={() => loggedIn ? handleLogout() : null}>
           {linkText}
         </NavLink>
+        ) : null}
       </nav>
-      ) : null}
     </div>
   );
 }
