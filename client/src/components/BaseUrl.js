7ce54e55a1e6763683f355bbf2dc86ccc27f569d
unknown <coding812@gmail.com>
2024-06-15 14:21:12 -0400

    re-structured project for azure

--- client/src/components/BaseUrl.js
+++ client/src/components/BaseUrl.js
@@ -1,4 +1,11 @@
-// const BaseUrl = 'http://localhost:5050';
+// if (process.env.NODE_ENV === 'development') {
+//     BaseUrl = 'http://localhost:5050';
+//   } 
+//   else {
+//     BaseUrl = 'https://spiffy.azurewebsites.net/';
+//   }
 
-const BaseUrl = 'https://spiffy.azurewebsites.net/';
-export default  BaseUrl;
+const BaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5050' : 'https://spiffy.azurewebsites.net/';
+
+export default BaseUrl;
+
